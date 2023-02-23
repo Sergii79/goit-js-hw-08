574cf31c42fcca3b621c5d29ef848b0e3ecf41af
Sergii79 <tsymbal.sergiy@gmail.com>
2023-02-23 13:40:44 +0200

    Commit third

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,12 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+// import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 console.log(galleryItems);
 console.log("Hello!!!");
 console.log("Hello world!!!");
@@ -27,3 +33,6 @@ const showGalleryItems = galleryItems
 .join("");
 
 galleryBox.insertAdjacentHTML("afterbegin", showGalleryItems);
+
+
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
