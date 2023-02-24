812e4c64408abbd0d007bce7d1f4c3212d5f2b95
Sergii79 <tsymbal.sergiy@gmail.com>
2023-02-24 21:41:56 +0200

    Commit fifth

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,24 +5,20 @@
 
 
     const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+    const player = new player(iframe);
 
     const onPlay = ({ seconds }) => {   
         localStorage.setItem("videoplayer-current-time", seconds);
-};
+    };
     
 
-    player.on('timeupdate', throttle (onPlay, 1000));
-    // player.on("timeupdate", throttle(function (data) {
-    //      const time = data.seconds;
-    //      console.log(time);
-    //     localStorage.setItem(TIME_DATA_PLAYER, time)
-    // },1000))
-
+    player.on('play', throttle(onPlay, 1000));
+    
+    
     player.setCurrentTime(localStorage.setItem("videoplayer-current-time"));
+    
+    
 
 
-
-
-
+   
 
