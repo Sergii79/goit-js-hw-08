9e90886436cb8d65177d3e664470977a3ca8b49a
Sergii79 <tsymbal.sergiy@gmail.com>
2023-02-24 14:06:20 +0200

    Commit third

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
 
+    import Player from '@vimeo/player';
+
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+    const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+// import throttle from 'lodash.throttle';
+// const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+// // console.log(localStorage);
+// document.addEventListener('touchstart', { passive: true });
