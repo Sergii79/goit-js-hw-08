f8e1016f32f49920376eeb344640e57859771ab9
Sergii79 <tsymbal.sergiy@gmail.com>
2023-02-24 15:36:57 +0200

    Commit fifth

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,28 @@
 
-    import Player from '@vimeo/player';
+    import player from '@vimeo/player';
+    
+    import throttle from 'lodash.throttle';
+
 
     const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+    const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
-
-    const onPlay = function(data) {
-    // data is an object containing properties specific to that event
+    const onPlay = ({ seconds }) => {   
+        localStorage.setItem("videoplayer-current-time", seconds);
 };
+    
 
-player.on('play', onPlay);
+    player.on('timeupdate', throttle (onPlay, 1000));
+    // player.on("timeupdate", throttle(function (data) {
+    //      const time = data.seconds;
+    //      console.log(time);
+    //     localStorage.setItem(TIME_DATA_PLAYER, time)
+    // },1000))
 
-// import throttle from 'lodash.throttle';
-// const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
-// // console.log(localStorage);
-// document.addEventListener('touchstart', { passive: true });
+    player.setCurrentTime(localStorage.setItem("videoplayer-current-time"));
+
+
+
+
+
+
