d98af8d03e519d5a04b10f1924703614c7eca71c
Sergii79 <tsymbal.sergiy@gmail.com>
2023-02-26 14:44:47 +0200

    Commit sixth

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,59 @@
+// Додаю бібліотеку як залежність проекту через npm https://github.com/vimeo/player.js/#vimeo-player-api
 
-    import player from '@vimeo/player';
+// Ініціалізуація плеєра https://github.com/vimeo/player.js/#pre-existing-player
+import Player from '@vimeo/player';
+
+console.log(Player);
+
+// Ініціалізуація бібліотеку lodash.throttle
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+
+// Ініціалізуація плеєра https://github.com/vimeo/player.js/#pre-existing-player
+const vimeoPlayer = new Player(iframe);
+
+// Відстеження події timeupdate https://github.com/vimeo/player.js/#events
+vimeoPlayer.on('timeupdate', throttle(onPlay, 1000, { leading: false }));
+
+currentTimeChecker();
+
+// Створюємо локальне сховище
+function onPlay(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+
+// Застосовуємо метод setCurrentTime() для відтворення останнього місця програвання https://github.com/vimeo/player.js/#setcurrenttimeseconds-number-promisenumber-rangeerrorerror
+function currentTimeChecker() {
+    if (getCurrentTime()) {
+        vimeoPlayer.setCurrentTime(getCurrentTime());
+    }
+}
+
+// функція повернення зі сховища значення з ключем 'videoplayer-current-time'
+function getCurrentTime() {
+    return localStorage.getItem('videoplayer-current-time');
+}
+
+
+
+
+
+    // import Player from '@vimeo/player';
     
-    import throttle from 'lodash.throttle';
+    // import throttle from 'lodash.throttle';
 
 
-    const iframe = document.querySelector('iframe');
-    const player = new player(iframe);
+    // const iframe = document.querySelector('iframe');
+    // const player = new Player(iframe);
 
-    const onPlay = ({ seconds }) => {   
-        localStorage.setItem("videoplayer-current-time", seconds);
-    };
+    // const onPlay = ({ seconds }) => {   
+    //     localStorage.setItem("videoplayer-current-time", seconds);
+    // };
     
 
-    player.on('play', throttle(onPlay, 1000));
+    // player.on('timeupdate', throttle(onPlay, 1000));
     
     
-    player.setCurrentTime(localStorage.setItem("videoplayer-current-time"));
-    
-    
-
-
-   
-
+    // player.setCurrentTime(localStorage.setItem("videoplayer-current-time"));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,61 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+
+form.addEventListener('input', throttle(historyUpdate, 500));
+
+form.addEventListener('submit', handleSubmitForm);
+
+
+const STORAGE_KEY = 'feedback-form-state';
+let feedbackFormState = {};
+
+historyCheck();
+
+function historyCheck() {
+  const currentValue = getFormCurrentValue();
+
+  if (currentValue?.email) {
+    feedbackFormState.email = form.email.value = currentValue.email;
+  }
+
+  if (currentValue?.message) {
+    feedbackFormState.message = form.message.value = currentValue.message;
+  }
+}
+
+function historyUpdate(event) {
+  feedbackFormState[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormState));
+}
+
+function handleSubmitForm(event) {
+  event.preventDefault();
+
+  if (!event.target.email.value || !event.target.message.value) {
+    return;
+  }
+
+  formDataOutput();
+  clearDataStorage();
+  event.currentTarget.reset();
+}
+
+function formDataOutput() {
+  if (getFormCurrentValue()) {
+    console.log(getFormCurrentValue());
+  }
+}
+
+function getFormCurrentValue() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY));
+}
+
+function clearDataStorage() {
+  if (getFormCurrentValue()) {
+    localStorage.removeItem(STORAGE_KEY);
+  }
+
+  feedbackFormState = {};
+}
